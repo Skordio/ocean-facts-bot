013c7c460ef371ba34353bb65132b63d3692d4b3
Skordio <skordion@hotmail.com>
2025-12-29 12:22:08 -0500

    improve api error logging

--- ocean_fact_api.py
+++ ocean_fact_api.py
@@ -1,20 +1,22 @@
 import requests, pprint, logging
 
 def get_ocean_fact():
+    # Initialize logging
     logging.basicConfig(filename='api.log', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S', format='%(asctime)s - %(levelname)s - %(message)s')
     error_bot_response = "Sorry, I couldn't find anything. Tell Skordio to go fix the API."
-    error_log_msg = "Error fetching ocean fact."
+
+    # Get stuff from server
     try:
         response = requests.get("https://oceanfacts.tropicbliss.net/api/random")
         if response.status_code == 200:
             logging.info("Successfully fetched ocean fact.")
             return response.json().get("fact", "No fact found.")
         else:
-            logging.error(f"{error_log_msg} Status code: {response.status_code}")
+            logging.error(f"HTTP error fetching ocean fact. Status code: {response.status_code}\nResponse text: {response.text}")
             return error_bot_response
     except Exception as e:
         # log to file
-        logging.error(f"{error_log_msg} Exception: {str(e)}")
+        logging.error(f"Error fetching ocean fact. Exception: {str(e)}")
         return error_bot_response
 
 
